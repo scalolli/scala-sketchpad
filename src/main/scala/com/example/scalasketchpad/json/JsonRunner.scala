35a50f6716a425ba2988a3e653dae3294668b64a
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-09-02 13:29:05 +0100

    tring out implicitly to understand it again :-\ I keep forgetting :-(

---
+++ src/main/scala/com/example/scalasketchpad/json/JsonRunner.scala
@@ -0,0 +1,7 @@
+package com.example.scalasketchpad.json
+
+object JsonRunner extends App {
+
+  val j: Json = Json.asJson("basu")
+  println(j)
+}
---
+++ src/main/scala/com/example/scalasketchpad/json/Jsonable.scala
@@ -0,0 +1,18 @@
+package com.example.scalasketchpad.json
+
+sealed abstract class Json
+private[json] case class JsonString(value: String) extends Json
+
+object Json {
+  def asJson[A: Encoder](a: A): Json = implicitly[Encoder[A]].encode(a)
+}
+
+trait Encoder[T] {
+  def encode(t: T): Json
+}
+
+object Encoder {
+  implicit def stringEncoder = new Encoder[String] {
+    override def encode(t: String): Json = JsonString(t)
+  }
+}
