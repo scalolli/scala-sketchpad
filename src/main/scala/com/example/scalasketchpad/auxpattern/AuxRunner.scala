ce7d7dcdcee0edbced64c741d4f3026ff04ab0c7
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-09-03 09:08:23 +0100

    Some more playing around with implicits and option

--- src/main/scala/com/example/scalasketchpad/auxpattern/AuxRunner.scala
+++ src/main/scala/com/example/scalasketchpad/auxpattern/AuxRunner.scala
@@ -16,10 +16,10 @@ object AuxRunner extends App {
       override def apply(tr: List[String]): Int = tr.size
     }
 
-//    implicit object optionInstance extends Unwrap[Option, Any] {
-//      override type Out = Boolean
-//      override def apply(tr: Option[Any]) = tr.isDefined
-//    }
+    implicit def optionInstance[A <: Any] = new Unwrap[Option, A] {
+      override type Out = Boolean
+      override def apply(tr: Option[A]) = tr.isDefined
+    }
   }
 
   trait PrettyPrinter[T] {
@@ -29,11 +29,22 @@ object AuxRunner extends App {
     implicit object IntPrettyPrinter extends PrettyPrinter[Int] {
       override def apply(t: Int): (Int, String) = (t, t.toString)
     }
+
+    implicit object BooleanPrettyPrinter extends PrettyPrinter[Boolean] {
+      override def apply(t: Boolean): (Boolean, String) = (t, t.toString)
+    }
   }
 
-  def extractor[T[_], R, Out](in: T[R])(implicit unwrap: Unwrap.Aux[T, R, Out], prettyPrinter: PrettyPrinter[Out]) = {
+  def extractAndPrint[T[_], R, Out](in: T[R])(implicit unwrap: Unwrap.Aux[T, R, Out],
+                                              prettyPrinter: PrettyPrinter[Out]) = {
     prettyPrinter(unwrap(in))
   }
 
-  println(extractor(List("abce", "def")))
+  def extractor[T[_], R, Out](in: T[R])(implicit unwrap: Unwrap.Aux[T, R, Out]) = {
+    unwrap(in)
+  }
+
+  println(extractAndPrint(List("abce", "def")))
+
+  println(extractor(Option("abcdef")))
 }
