dfcbe316a55e3450b4b38af713190b1a8099e659
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2018-10-23 09:11:59 +0100

    JSON parsing - how Circe does it

--- src/main/scala/com/example/scalasketchpad/json/JsonRunner.scala
+++ src/main/scala/com/example/scalasketchpad/json/JsonRunner.scala
@@ -4,4 +4,7 @@ object JsonRunner extends App {
 
   val j: Json = Json.asJson("basu")
   println(j)
+
+  val listJson = Json.asJson(Seq("Basu", "1"))
+  println(listJson)
 }
--- src/main/scala/com/example/scalasketchpad/json/Jsonable.scala
+++ src/main/scala/com/example/scalasketchpad/json/Jsonable.scala
@@ -1,7 +1,9 @@
 package com.example.scalasketchpad.json
 
 sealed abstract class Json
-private[json] case class JsonString(value: String) extends Json
+private[json] case class JsonString(value: String)   extends Json
+private[json] case class JsonNumber(value: Int)      extends Json
+private[json] case class JsonList(values: Seq[Json]) extends Json
 
 object Json {
   def asJson[A: Encoder](a: A): Json = implicitly[Encoder[A]].encode(a)
@@ -14,5 +16,12 @@ trait Encoder[T] {
 object Encoder {
   implicit object StringEncoder extends Encoder[String] {
     override def encode(t: String): Json = JsonString(t)
+  }
+  implicit object IntEncoder extends Encoder[Int] {
+    override def encode(t: Int): Json = JsonNumber(t)
+  }
+
+  implicit def SeqEncoder[T: Encoder] = new Encoder[Seq[T]] {
+    override def encode(t: Seq[T]): Json = JsonList(t.map(implicitly[Encoder[T]].encode(_)))
   }
 }
